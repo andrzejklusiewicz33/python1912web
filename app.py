6ae6fe8d812d0eeb7da320d966e13ade405efe71
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-22 14:08:12 +0100

    zadanie

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'tekst zmieniony'
+    return '<h1>Strona główna</h1>'
+
+@app.route('/tests')
+def tests():
+    return "<h1>Testy</h1>"
 
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
 
+
+#BEaUTIFULSOUP
+#SCRAPY
 #58. Uruchom aplikację w trybie debug na porcie 80 (albo 8080)
+#59. Stwórz metody które będą obsługiwały ekrany: /show_products, /show_employees, /about  i /
+#Metody te powinny spowodować wyświetlenie odpowiedniego tekstu
