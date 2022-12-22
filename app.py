430f5c2a98f840e3a85a19797d772b5fc525ab86
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-22 14:22:13 +0100

    zadanie

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -7,9 +7,20 @@ app = Flask(__name__)
 def index():
     return '<h1>Strona główna</h1>'
 
+@app.route('/show_products')
+def show_products():
+    return "<h1>Lista produktów</h1>"
+
+@app.route('/show_employees')
+def show_employees():
+    return "<h1>Lista pracowników</h1>"
+@app.route("/about")
+def about():
+    return "<h1>Strona o programie</h1>"
 @app.route('/tests')
 def tests():
-    return "<h1>Testy</h1>"
+    return render_template('tests.html')
+    #return "<h1>Testy</h1>"
 
 
 if __name__ == '__main__':
@@ -21,3 +32,5 @@ if __name__ == '__main__':
 #58. Uruchom aplikację w trybie debug na porcie 80 (albo 8080)
 #59. Stwórz metody które będą obsługiwały ekrany: /show_products, /show_employees, /about  i /
 #Metody te powinny spowodować wyświetlenie odpowiedniego tekstu
+
+#60. Zadbaj o to by kazdy ekran powodowal wyswietlenie innego pliku html z jakims naglowkiem
